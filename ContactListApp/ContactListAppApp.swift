a1a211a393f0d53cae3f803ad2c5ca97e491f133
Anna <anna.vasi4ko@icloud.com>
2022-03-02 01:50:32 +0300

    Initial Commit

---
+++ ContactListApp/ContactListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListAppApp.swift
+//  ContactListApp
+//
+//  Created by Vasichko Anna on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
